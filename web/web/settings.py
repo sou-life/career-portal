f25d8c92264165a910bee9866bea2656c747e841
sou-life <soumyas9025@gmail.com>
2025-05-05 20:35:57 +0530

    Prepare for deployment

--- web/web/settings.py
+++ web/web/settings.py
@@ -20,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8jazpfd_626gxj+ctbl&+o(c7w36eyz_wr4(qaj%7y&o%!%$_*'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-default-key")
 
-ALLOWED_HOSTS = ['careerportal.onrender.com'
-]
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "localhost")]
 
 
 # Application definition
